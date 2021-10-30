01de14c13b96381cbfa7593584dade813a863b58
vishaljadhav <vishalljadhav86@gmail.com>
2021-10-30 16:32:39 +0530

    initial

--- demo.js
+++ demo.js
@@ -1,1 +1,8 @@
 console.log("done");
+ <html> <body>
+      <p>Before the script...</p> 
+       <script> alert( 'Hello, world!' ); 
+           </script> <p>...After the script.</p> 
+           </body>
+            </html>
+
