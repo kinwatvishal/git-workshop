895412fc8c15c9d94c5e9ce3d70f37730bdc872d
vishaljadhav <vishalljadhav86@gmail.com>
2021-11-01 15:24:00 +0530

    commit

--- demo.js
+++ demo.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-console.log("done work");
-console.log("hello wod");
-=======
-console.log("done");
- <html> <body>
-      <p>Before the script...</p> 
-       <script> alert( 'Hello, world!' ); 
-           </script> <p>...After the script.</p> 
-           </body>
-            </html>
 
->>>>>>> 01de14c13b96381cbfa7593584dade813a863b58
+console.log("done work");
+console.log("i am master branch");
+
+ 
